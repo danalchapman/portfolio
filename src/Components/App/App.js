c7bebab4275c2ce1ec1eb7198641177f9813cda9
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-02-15 13:03:10 -0600

    Connect project data and apply grid styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,27 @@
+import { useState, useEffect } from 'react'
 import { Nav } from '../Nav/Nav'
 import { About } from '../About/About'
 import { Projects } from '../Projects/Projects'
 import { Connect } from '../Connect/Connect'
 import { FindMe } from '../FindMe/FindMe'
+import { projectData } from '../../projectData'
 import './App.css';
 
 export const App = () => {
+
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    setProjects(projectData)
+  })
+
   return (
     <main className='main-container'>
       <Nav />
       <About />
-      <Projects />
+      <Projects 
+        projects={ projects }
+      />
       <Connect />
       <FindMe />
     </main>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,9 +1,28 @@
+import { ProjectCard } from '../ProjectCard/ProjectCard'
 import './Projects.css'
 
-export const Projects = () => {
+export const Projects = ({ projects }) => {
+
+    const displayProjects = () => {
+        return projects.map(project => {
+            return (
+                <ProjectCard 
+                    key={ project.projectID }
+                    title={ project.title }
+                    repoLink={ project.repoLink }
+                    deployLink={ project.deployLink }
+                    summary={ project.summary }
+                />
+            )
+        })
+    }
+
     return (
         <section className='projects-container'>
             <h3>Projects</h3>
+            <article className='card-box'>
+                { displayProjects() }
+            </article>
         </section>
     )
 }

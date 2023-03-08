df15412515dcc833bc9b5497cc4955ba84562da9
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-08 11:12:53 -0600

    Stylize nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,18 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './Nav.css'
+
+const logo = require('../../Assets/7934292.png')
 
 export const Nav = () => {
     return (
         <nav className='nav-container'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/projects'>Projects</Link>
-            <Link to='/connect'>Connect</Link>
+            <img src={logo} alt='logo'className='nav-logo'/>
+            <div className='nav-links'>
+                <NavLink className='inactive' to='/'>Home</NavLink>
+                <NavLink className='inactive' to='/about'>About</NavLink>
+                <NavLink className='inactive' to='/projects'>Projects</NavLink>
+                <NavLink className='inactive' to='/connect'>Connect</NavLink>
+            </div>
         </nav>
     )
 }

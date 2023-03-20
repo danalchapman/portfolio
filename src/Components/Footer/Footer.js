d13b74e609260408080c8f8bfabeb01b87c814b8
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-20 14:43:03 -0500

    Add titles to footer links and minor styling adjustment

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,11 +8,21 @@ import './Footer.css'
 export const Footer = () => {
     return (
         <footer className='footer-container'>
-            <img src={email} alt='icon' className='icon'/>
-            <img src={github} alt='icon' className='icon'/>
-            <img src={insta} alt='icon' className='icon'/>
-            <img src={linkedin} alt='icon' className='icon'/>
-            <img src={twitter} alt='icon' className='icon'/>
+            <a href='mailto:dana.leigh.dlc@gmail.com?subject=Coffee%20Chat' className='icon-link' title='Email'>
+                <img src={email} alt='icon' className='icon'/>
+            </a>
+            <a href='https://github.com/danalchapman' className='icon-link' title='GitHub'>
+                <img src={github} alt='icon' className='icon'/>
+            </a>
+            <a href='https://www.instagram.com/danaleighbee/' className='icon-link' title='Instagram'>
+                <img src={insta} alt='icon' className='icon'/>
+            </a>
+            <a href='https://www.linkedin.com/in/danalchapman/' className='icon-link' title='LinkedIn'>
+                <img src={linkedin} alt='icon' className='icon'/>
+            </a>
+            <a href='https://twitter.com/danaleighbee' className='icon-link' title='Twitter'>
+                <img src={twitter} alt='icon' className='icon'/>    
+            </a>
         </footer>
     )
 }

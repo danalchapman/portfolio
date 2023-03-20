1e4d70cf1f90fef8eacaa921d182b08ca550492d
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-20 16:37:25 -0500

    Style projects grid

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,11 +1,21 @@
+import filler from '../../Assets/space-img.avif'
 import './ProjectCard.css'
 
 export const ProjectCard = ({ title, repoLink, deployLink }) => {
     return (
         <article className='project-card'>
-            <h4>{ title }</h4>
-            <p><a href={ deployLink }>View</a></p>
-            <p><a href={ repoLink }>Repository</a></p>
+            <img 
+                src={ filler }
+                alt='filler image'
+                className='card-img'
+            />
+            <div>
+                <h4>{ title }</h4>
+                <div className='card-links'>
+                    <p><a href={ deployLink } title='Deployed Site'>View</a></p>
+                    <p><a href={ repoLink } title='Repository'>Repository</a></p>
+                </div>
+            </div>
         </article>
     )
 }

fc75c9e7626772ed79447a015eeaf01ec0882dbd
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-20 13:10:11 -0500

    Add quote and author block to home component

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <img />
+            <img />
+            <img />
+            <img />
+            <img />
+        </footer>
+    )
+}

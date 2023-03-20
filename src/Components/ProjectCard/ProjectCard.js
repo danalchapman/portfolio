cedb21fc98eec1a690ff4e33a7a87bd76c756e16
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-20 15:54:10 -0500

    Build Projects component skeleton and styling

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,12 +1,11 @@
 import './ProjectCard.css'
 
-export const ProjectCard = ({ title, repoLink, deployLink, summary }) => {
+export const ProjectCard = ({ title, repoLink, deployLink }) => {
     return (
         <article className='project-card'>
             <h4>{ title }</h4>
-            <p>{ summary }</p>
-            <p><a href={ deployLink }>View the Project</a></p>
-            <p><a href={ repoLink }>Repository on GitHub</a></p>
+            <p><a href={ deployLink }>View</a></p>
+            <p><a href={ repoLink }>Repository</a></p>
         </article>
     )
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,8 @@
+import { Link } from 'react-router-dom'
 import { ProjectCard } from '../ProjectCard/ProjectCard'
+import { Footer } from '../Footer/Footer'
+
+import lines from '../../Assets/lines.avif'
 import './Projects.css'
 
 export const Projects = ({ projects }) => {
@@ -11,17 +15,29 @@ export const Projects = ({ projects }) => {
                     title={ project.title }
                     repoLink={ project.repoLink }
                     deployLink={ project.deployLink }
-                    summary={ project.summary }
                 />
             )
         })
     }
 
     return (
-        <section className='projects-container'>
-            <article className='card-box'>
-                { displayProjects() }
-            </article>
-        </section>
+        <div className='projects-container'>
+            <img 
+                src={ lines }
+                alt='hazy lines over a dark background'
+                className='projects-left'
+            />
+            <section className='projects-right'>
+                <div className='projects-nav-container'>
+                    <Link to='/story'><button className='projects-btns'>Story</button></Link>
+                    <Link to='/connection'><button className='projects-btns'>Connection</button></Link>
+                </div>
+                <div className='card-box'>
+                    { displayProjects() }
+                </div>
+                <Link to='/'><button className='projects-btns'>Home</button></Link>
+                <Footer />
+            </section>
+        </div>
     )
 }

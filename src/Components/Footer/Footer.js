79297fd48c30cf933c14a9e219cefcfdd77447fb
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-20 13:38:13 -0500

    Stylize footer with icons and add cursor pointer to buttons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,18 @@
+import email from '../../Assets/gmail.png'
+import github from '../../Assets/github(1).png'
+import insta from '../../Assets/instagram.png'
+import linkedin from '../../Assets/linkedin(1).png'
+import twitter from '../../Assets/twitter.png'
+import './Footer.css'
+
 export const Footer = () => {
     return (
-        <footer>
-            <img />
-            <img />
-            <img />
-            <img />
-            <img />
+        <footer className='footer-container'>
+            <img src={email} alt='icon' className='icon'/>
+            <img src={github} alt='icon' className='icon'/>
+            <img src={insta} alt='icon' className='icon'/>
+            <img src={linkedin} alt='icon' className='icon'/>
+            <img src={twitter} alt='icon' className='icon'/>
         </footer>
     )
 }

55e1d78ba9a8fea4dcb8791a4f6a6b2172b65143
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-29 15:18:28 -0500

    Remove Story component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { Home } from '../Home/Home'
-// import { Story } from '../Story/Story'
 import { Projects } from '../Projects/Projects'
 import { Connection } from '../Connection/Connection'
 
@@ -22,7 +21,6 @@ export const App = () => {
     <main className='main-container'>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/story' element={<Story />} /> */}
         <Route path='/projects' element={<Projects projects={ projects } />} />
         <Route path='/connection' element={<Connection />} />
       </Routes>
